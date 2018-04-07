4075b6973fc1785f96912ad0a5dd2e72c917d54f
ZiCKeZaCKe90 <38163179+ZiCKeZaCKe90@users.noreply.github.com>
2018-04-07 18:56:37 +0200

    Add basic scene setup

---
+++ Fluch der Akribik/Assets/Scripts/ShipNavigation.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class ShipNavigation : MonoBehaviour {
+
+	public Camera cam;
+
+	public NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () 
+	{
+		if (Input.GetMouseButtonDown (0)) 
+		{
+			Ray ray = cam.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;
+
+			if (Physics.Raycast(ray, out hit))
+			{
+				agent.SetDestination (hit.point);
+				Debug.Log ("Hit!");
+			}
+		}	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
